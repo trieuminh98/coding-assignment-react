f2fe05176a77ea51e7aa66397ed2647859dc00f7
Gawain <gawain@brickmate.vn>
2024-02-22 13:22:49 +0700

    (U) Update tickets page

---
+++ client/src/app/tickets/ticket-details.spec.tsx
@@ -0,0 +1,137 @@
+import { Box, Button, TextField } from '@mui/material';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { Dialog } from '../elements';
+import { useDialog, usePostData, useQuery } from '../hooks';
+import Tickets from './tickets';
+
+jest.mock('../hooks');
+const mockPostData = jest.mocked(usePostData);
+const mockQuery = jest.mocked(useQuery);
+const mockDialog = jest.mocked(useDialog);
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(),
+  })
+) as jest.Mock;
+
+describe('TicketDetails', () => {
+  const mockPostFn = jest.fn();
+
+  beforeAll(() => {
+    mockPostData.mockReturnValue({
+      post: mockPostFn,
+      isPosting: false,
+    });
+
+    mockDialog.mockReturnValue({
+      open: false,
+      onCloseDialog: jest.fn(),
+      onOpenDialog: jest.fn(),
+    });
+
+    mockQuery.mockReturnValue({
+      data: [
+        { id: 1, description: 'Ticket 1', completed: true },
+        { id: 2, description: 'Ticket 2', completed: false },
+      ],
+      isFetching: false,
+      refetch: jest.fn(),
+    });
+  });
+
+  it('should render successfully', () => {
+    const { baseElement } = render(
+      <Router>
+        <Tickets />
+      </Router>
+    );
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should filter tickets based on COMPLETE stats', async () => {
+    jest.mock('../hooks', () => ({
+      useQuery: () => ({
+        data: [
+          { id: 1, description: 'Completed Ticket', completed: true },
+          { id: 2, description: 'Incomplete Ticket', completed: false },
+        ],
+        isFetching: false,
+      }),
+    }));
+
+    const { getByTestId, findByText } = render(
+      <Router>
+        <Tickets />
+      </Router>
+    );
+
+    const select = await waitFor(() => getByTestId('status-select'));
+
+    fireEvent.change(select, { target: { value: 'COMPLETE' } });
+
+    expect(await findByText('Complete')).toBeInTheDocument();
+  });
+
+  it('should filter tickets based on INCOMPLETE status', async () => {
+    jest.mock('../hooks', () => ({
+      useQuery: () => ({
+        data: [
+          { id: 1, description: 'Completed Ticket', completed: true },
+          { id: 2, description: 'Incomplete Ticket', completed: false },
+        ],
+        isFetching: false,
+      }),
+    }));
+
+    const { getByTestId, findByText } = render(
+      <Router>
+        <Tickets />
+      </Router>
+    );
+
+    const select = await waitFor(() => getByTestId('status-select'));
+
+    fireEvent.change(select, { target: { value: 'INCOMPLETE' } });
+
+    expect(await findByText('Incomplete')).toBeInTheDocument();
+  });
+
+  it('should allow adding a new ticket', async () => {
+    const { getByText } = render(
+      <Router>
+        <Tickets />
+      </Router>
+    );
+
+    fireEvent.click(getByText('Add Ticket'));
+
+    render(
+      <Router>
+        <Dialog
+          title="Add Ticket"
+          open={true}
+          handleClose={() => {
+            console.log('mock');
+          }}
+        >
+          <TextField />
+          <Box display={'flex'} gap={2} mt={2}>
+            <Button
+              onClick={mockPostFn}
+              fullWidth
+              size="medium"
+              variant="contained"
+            >
+              Add
+            </Button>
+          </Box>
+        </Dialog>
+      </Router>
+    );
+
+    fireEvent.click(getByText('Add'));
+    expect(mockPostFn).toHaveBeenCalled();
+  });
+});
---
+++ client/src/app/tickets/tickets.tsx
@@ -0,0 +1,195 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Ticket, User } from '@acme/shared-models';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import EditIcon from '@mui/icons-material/Edit';
+import {
+  Box,
+  Button,
+  Card,
+  FormControl,
+  IconButton,
+  InputLabel,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BackdropLoading, Dialog, Loading } from '../elements';
+import { useDialog, usePostData, useQuery } from '../hooks';
+import { POST_DATA_METHOD } from '../services/request.service';
+
+enum STATUS {
+  ALL = 'ALL',
+  COMPLETE = 'COMPLETE',
+  INCOMPLETE = 'INCOMPLETE',
+}
+
+const TICKET_STATUS_DATA = [
+  {
+    value: STATUS.ALL,
+    label: 'All',
+  },
+  {
+    value: STATUS.COMPLETE,
+    label: 'Complete',
+  },
+  {
+    value: STATUS.INCOMPLETE,
+    label: 'Incomplete',
+  },
+];
+
+export function Tickets() {
+  const [filterStatus, setFilterStatus] = useState<STATUS>(STATUS.ALL);
+  const [description, setDescription] = useState('');
+  const navigate = useNavigate();
+  const { open, onCloseDialog, onOpenDialog } = useDialog();
+  const {
+    data: ticket = [],
+    refetch,
+    isFetching,
+  } = useQuery<Ticket[]>(`/api/tickets`);
+
+  const { data: users = [] } = useQuery<User[]>(`/api/users`);
+  const [err, setErr] = useState(false);
+  const { post, isPosting } = usePostData(POST_DATA_METHOD.POST);
+  const ticketsFilterByStatus = useMemo(() => {
+    const isCompleted = filterStatus === STATUS.COMPLETE;
+    return filterStatus === STATUS.ALL
+      ? ticket
+      : ticket.filter((i) => i.completed === isCompleted);
+  }, [filterStatus, ticket]);
+
+  const onNavigateToDetail = (id: number) => {
+    navigate(`/${id}`);
+  };
+
+  const reset = () => {
+    setErr(false);
+    setDescription('');
+  };
+
+  const onAdd = async () => {
+    if (!description) {
+      setErr(true);
+      return;
+    }
+    await post(`/api/tickets`, {
+      description,
+    });
+    reset();
+    onCloseDialog();
+
+    await refetch();
+  };
+
+  return (
+    <Box>
+      <Box display={'flex'} justifyContent={'space-between'}>
+        <Box component={'h1'}>Ticket List</Box>
+        <IconButton
+          sx={{
+            gap: 2,
+            '&:hover': {
+              background: 'white',
+            },
+          }}
+          color="primary"
+          aria-label="delete"
+          onClick={onOpenDialog}
+        >
+          <AddCircleIcon />
+          <Typography>Add Ticket</Typography>
+        </IconButton>
+      </Box>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Status</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={filterStatus}
+          inputProps={{
+            id: 'status-select',
+            'data-testid': 'status-select',
+          }}
+          label="Status"
+          sx={{ width: 300 }}
+          onChange={(e) => setFilterStatus(e.target.value as STATUS)}
+        >
+          {TICKET_STATUS_DATA.map((i) => (
+            <MenuItem key={i.value} value={i.value}>
+              {i.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Card sx={{ p: 3, width: 700, mt: 2 }}>
+        <List
+          sx={{
+            bgcolor: 'background.paper',
+          }}
+        >
+          {ticketsFilterByStatus.map((value, index) => {
+            const labelId = `checkbox-list-label-${value}`;
+
+            return (
+              <ListItem key={value.id} disablePadding>
+                <ListItemButton
+                  onClick={() => {
+                    onNavigateToDetail(value.id);
+                  }}
+                  dense
+                >
+                  <ListItemText
+                    id={labelId}
+                    primary={
+                      <Box display={'flex'} justifyContent={'space-between'}>
+                        <Typography>
+                          {index + 1}: {value.description}
+                        </Typography>
+                        <Box gap={4} display={'flex'}>
+                          <Typography>
+                            {users.find((i) => i.id === value.assigneeId)?.name}
+                          </Typography>
+                          <EditIcon />
+                        </Box>
+                      </Box>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Card>
+      <Dialog title="Add Ticket" open={open} handleClose={onCloseDialog}>
+        <TextField
+          sx={{ width: 400 }}
+          error={err}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          id="outlined-basic"
+          label="Ticket Description"
+          variant="outlined"
+        />
+        <Box display={'flex'} gap={2} mt={2}>
+          <Button onClick={reset} fullWidth size="medium" variant="outlined">
+            Cancel
+          </Button>
+          <Button onClick={onAdd} fullWidth size="medium" variant="contained">
+            {isPosting ? <Loading color="inherit" size={20} /> : 'Add'}
+          </Button>
+        </Box>
+      </Dialog>
+      <BackdropLoading open={isFetching} />
+    </Box>
+  );
+}
+
+export default Tickets;
